743b7046365506fdced7ca9d1b3be65e680dd143
vinayak-naik <vinayaknaik2021@gmail.com>
2022-12-03 10:25:52 +0530

    first commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+// const MY_SECRETE = process.env.MY_SECRETE || "";
+
+export default NextAuth({
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorizationUrl:
+        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
+    }),
+  ],
+  callbacks: {
+    async session(session) {
+      if (!session) session;
+
+      // const name = session ? session.user?.name : "";
+      // const email = session ? session.user?.email : "";
+
+      // //Encrypting privateKey
+      // const token = jwt.sign({ name, email }, MY_SECRETE);
+      // session.token = token;
+      return session;
+    },
+    redirect: async (url) => {
+      if (url === "/user") {
+        return Promise.resolve("/");
+      }
+      return Promise.resolve("/");
+    },
+  },
+});
