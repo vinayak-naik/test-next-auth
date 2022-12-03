55f56e6f1b99bbb0cefbfd3bc5487270d9c861ec
vinayak-naik <vinayaknaik2021@gmail.com>
2022-12-03 11:30:52 +0530

    first commit12

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider as AuthProvider } from "next-auth/client";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return<>
+  <AuthProvider session={pageProps.session}>
+   <Component {...pageProps} />
+  </AuthProvider>
+  </>
+  
 }
